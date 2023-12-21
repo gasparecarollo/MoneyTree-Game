2a9dc76311e583cc5b20ec74d4a45d52a146b47c
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-12-21 17:13:01 -0500

    Added Score counter, added GameOver, added Coin and BGMusic sound

--- src/main.jsx
+++ src/main.jsx
@@ -20,12 +20,18 @@ class GameScene extends Phaser.Scene {
     this.points = 0;
     this.textScore;
     this.textTime;
+    this.timedEvent;
+    this.remainingTime;
   }
+
   preload() {
     this.load.image("bg", "/public/vite.svg")
     this.load.image("basket", "/assets/basket.png")
     this.load.image("apple", "/assets/apple.png")
+    this.load.audio("coin", "/assets/coin.mp3")
+    this.load.audio("bgMusic", "/assets/bgMusic.mp3")
   }
+
   create() {
     this.add.image(0, 0, "bg").setOrigin(0, 0)
     this.player = this.physics.add.image(0, sizes.height - 100, "basket").setOrigin(0, 0)
@@ -51,9 +57,12 @@ class GameScene extends Phaser.Scene {
       font: "25px Arial",
       fill: "#000000",
     });
-
+    this.timedEvent = this.time.delayedCall(3000, this.gameOver(), [], this)
   }
+
   update() {
+    this.remainingTime = this.timedEvent.getRemainingSeconds()
+    this.textTime.setText(`Remaining Time: ${Math.round(this.remainingTime).toString()}`)
 
     if (this.target.y >= sizes.height) {
       this.target.setY(0);
@@ -78,6 +87,9 @@ class GameScene extends Phaser.Scene {
     this.target.setX(this.getRandomX());
     this.points++;
     this.textScore.setText(`Score: ${this.points}`)
+  }
+  gameOver() {
+    console.log("Game Over")
   }
 }
 
